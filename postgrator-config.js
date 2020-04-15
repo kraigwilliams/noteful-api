d307ea42f195ed0e64fce69c0ed58bed73b61363
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-15 09:52:57 -0400

    test

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 
-module.exports={
-'migrationsDirectory':'migrations',
-'driver':'pg',
-'connectionString': (process.env.NODE_ENV==='test')
-? process.env.TEST_DATABASE_URL
-: process.env.DATABASE_URL,
-"ssl": !!process.env.SSL
+module.exports = {
+  "validateChecksums": "false",
+  "migrationDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": (process.env.NODE_ENV === 'test')
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  
+ "ssl": true,
+
+  "host": process.env.MIGRATION_DATABASE_HOST,
+  "port": process.env.MIGRATION_DATABASE_PORT,
+  "database": process.env.MIGRATION_DATABASE_NAME,
+  "username": process.env.MIGRATION_DATABASE_USER,
+  "password": process.env.MIGRATION_DATABASE_PASS
 }
